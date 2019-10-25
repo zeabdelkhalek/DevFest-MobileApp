09ea8faf54cf5cf756fef5ff913b27224cd250a1
zellat abdelkhalek <ha_zellat@esi.dz>
2019-10-25 17:52:05 +0100

    add card component ( slider not working )

--- src/screens/mainScreen.js
+++ src/screens/mainScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text , StyleSheet} from 'react-native';
 
 class mainScreen extends Component {
   constructor(props) {
@@ -12,9 +12,28 @@ class mainScreen extends Component {
     return (
       <View>
         <Text> mainScreen z zdz </Text>
+        <View style={styles.fab}>
+          <Text>dfjk</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  fab : {
+    width : 40 , 
+    height : 40 , 
+    position : 'absolute',
+    right : 18 , 
+    bottom : 70,
+    backgroundColor : '#FFB53E' , 
+    borderRadius: 5,
+    shadowColor: '#00000029',
+			shadowOffset: { width: 0, height: 3 },
+			shadowOpacity: 6,
+			shadowRadius: 1,
+  }
+});
 
 export default mainScreen;
--- src/screens/mapScreen.js
+++ src/screens/mapScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import CardComponent from "../components/CardComponent";
+import PlusIcon from "../assets/icons/plus.svg"
+// import CardSilder from 'react-native-cards-slider';
 
 class mapScreen extends Component {
   constructor(props) {
@@ -10,11 +13,41 @@ class mapScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text> mapScrseen </Text>
+      <View style={{ height: '100%', width: '100%' }} >
+        <CardSilder>
+          <CardComponent />
+          <CardComponent />
+          <CardComponent />
+          <CardComponent />
+          <CardComponent />
+          <CardComponent />
+          <CardComponent />
+          <CardComponent />
+        </CardSilder>
+        <View style={styles.fab}>
+          <PlusIcon fill={'#fff'} style={{ width: 16, height: 16 }} />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  fab: {
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 18,
+    bottom: 20,
+    backgroundColor: '#FFB53E',
+    borderRadius: 5,
+    shadowColor: '#00000029',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 6,
+    shadowRadius: 1,
+  }
+});
 
 export default mapScreen;
